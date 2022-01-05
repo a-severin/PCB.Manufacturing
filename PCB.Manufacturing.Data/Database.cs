8da6ff1cf74c9121e2617c8d86f0f7f505838c38
Anatoly Severin <severin@pointcad.ru>
2022-01-05 12:55:50 +0300

    splits solution on projects

---
+++ PCB.Manufacturing.Data/Database.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using PCB.Manufacturing.Model;
+
+namespace PCB.Manufacturing.Data;
+
+public class Database
+{
+    public IEnumerable<Material> Materials()
+    {
+        return new[]
+        {
+            new Material
+            {
+                Id = 1,
+                Name = "Arlon",
+                ExtraMoney = 160,
+                ExtraTime = 0
+            },
+            new Material
+            {
+                Id = 2,
+                Name = "Carbon",
+                ExtraMoney = 260,
+                ExtraTime = 3
+            },
+            new Material
+            {
+                Id = 3,
+                Name = "Wolfram",
+                ExtraMoney = 560,
+                ExtraTime = 7
+            },
+        };
+    }
+}
---
+++ PCB.Manufacturing.Model/ProjectInfo.cs
@@ -0,0 +1,76 @@
+﻿using System.Net.Http.Headers;
+
+namespace PCB.Manufacturing.Model;
+
+public class ProjectInfo
+{
+    public string ProjectName { get; set; }
+    public string Zipcode { get; set; }
+    public int BoardsQuantity { get; set; }
+}
+
+public struct Material
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ExtraMoney { get; set; }
+    public int ExtraTime { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Material material
+               && material.Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id;
+    }
+}
+
+public struct Surface
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ExtraMoney { get; set; }
+    public int ExtraTime { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Surface surface
+               && surface.Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id;
+    }
+}
+
+public struct SolderMaskColor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public short R { get; set; }
+    public short G { get; set; }
+    public short B { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is SolderMaskColor solderMaskColor
+               && solderMaskColor.Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id;
+    }
+}
+
+public class BoardInfo
+{
+    public double BoardThickness { get; set; }
+    public Material Material { get; set; }
+    public Surface Surface { get; set; }
+    public SolderMaskColor SolderMaskColor { get; set; }
+}
